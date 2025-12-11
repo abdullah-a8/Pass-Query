d0a0e1be3090029d8b4159f8be7cb36b002edd87
Abdullah <github@corestack.simplelogin.com>
2025-12-11 19:38:56 +0500

    feat: add CLI argument parsing with clap

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
+use clap::Parser;
+
 mod models;
 mod pass_cli;
 mod search;
 mod selection;
 
+/// Fast Proton Pass password search
+#[derive(Parser)]
+#[command(name = "pp")]
+#[command(about = "Search Proton Pass and print password to stdout")]
+#[command(version)]
+struct Cli {
+    /// Search query (item name)
+    query: String,
+}
+
 fn main() {
+    let _cli = Cli::parse();
     println!("Hello, world!");
 }
